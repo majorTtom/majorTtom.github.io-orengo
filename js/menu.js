2d065bae189ec9f20b2d3ef704f94f8dcb616707
majorTtom <69448481+majorTtom@users.noreply.github.com>
2022-04-27 02:56:49 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-  $(document).on('click', '.menu__btn', function(e){
+   $(document).on('click', '.menu__btn', function(e){
     e.preventDefault;
     $('.menu__btn-active').removeClass('menu__btn-active');
     $('.menu__first').removeClass('menu__first-active');
     $('.header').removeClass('header__active');
     $(this).addClass('menu__btn-active');  
   })
+
    /*ПОРЦИОННЫЕ*/
    /*golubika*/
   $(document).on('mouseover', '.tg', function(e){
@@ -69,14 +70,14 @@ $(document).ready(function(){
 
   /*ЗАКУСКИ*/
   /*сэндв с семгой(ssem)*/
-  $(document).on('mouseover', '.ssem', function(e){
+  $(document).on('mouseenter', '.ssem', function(e){
     e.preventDefault;
     $('.ssem-t').addClass('menu__side-active');
     $('.menu__side').addClass('menu__side-active');
     $('.menu').addClass('menu__active');
   }
 
-  $(document).on('mouseout', '.ssem', function(e){
+  $(document).on('mouseleave', '.ssem', function(e){
     e.preventDefault;
     $('.ssem-t').removeClass('menu__side-active');
     $('.menu__side').removeClass('menu__side-active');
@@ -84,141 +85,16 @@ $(document).ready(function(){
   }
 
   /*блинчики с творгом и джемом(btd)*/
-  $(document).on('mouseover', '.btd', function(e){
+  $(document).on('mouseenter', '.btd', function(e){
     e.preventDefault;
     $('.btd-t').addClass('menu__side-active');
     $('.menu__side').addClass('menu__side-active');
     $('.menu').addClass('menu__active');
   }
 
-  $(document).on('mouseout', '.btd', function(e){
+  $(document).on('mouseleave', '.btd', function(e){
     e.preventDefault;
     $('.btd-t').removeClass('menu__side-active');
-    $('.menu__side').removeClass('menu__side-active');
-    $('.menu').removeClass('menu__active');
-  }
-
-  /*ТОРТЫ*/
-  /*меренговый рулет(mr)*/
-  $(document).on('mouseover', '.mr', function(e){
-    e.preventDefault;
-    $('.mr-t').addClass('menu__side-active');
-    $('.menu__side').addClass('menu__side-active');
-    $('.menu').addClass('menu__active');
-  }
-
-  $(document).on('mouseout', '.mr', function(e){
-    e.preventDefault;
-    $('.mr-t').removeClass('menu__side-active');
-    $('.menu__side').removeClass('menu__side-active');
-    $('.menu').removeClass('menu__active');
-  }
-
-  /*наролеон(nap)*/
-  $(document).on('mouseover', '.nap', function(e){
-    e.preventDefault;
-    $('.nap-t').addClass('menu__side-active');
-    $('.menu__side').addClass('menu__side-active');
-    $('.menu').addClass('menu__active');
-  }
-
-  $(document).on('mouseout', '.nap', function(e){
-    e.preventDefault;
-    $('.nap-t').removeClass('menu__side-active');
-    $('.menu__side').removeClass('menu__side-active');
-    $('.menu').removeClass('menu__active');
-  }
-
-  /*ГРАНОЛА*/
-  /*gr1*/
-  $(document).on('mouseover', '.gr1', function(e){
-    e.preventDefault;
-    $('.gr1-t').addClass('menu__side-active');
-    $('.menu__side').addClass('menu__side-active');
-    $('.menu').addClass('menu__active');
-  }
-
-  $(document).on('mouseout', '.gr1', function(e){
-    e.preventDefault;
-    $('.gr1-t').removeClass('menu__side-active');
-    $('.menu__side').removeClass('menu__side-active');
-    $('.menu').removeClass('menu__active');
-  }
-
-  /*gr6*/
-  $(document).on('mouseover', '.gr6', function(e){
-    e.preventDefault;
-    $('.gr6-t').addClass('menu__side-active');
-    $('.menu__side').addClass('menu__side-active');
-    $('.menu').addClass('menu__active');
-  }
-
-  $(document).on('mouseout', '.gr6', function(e){
-    e.preventDefault;
-    $('.gr6-t').removeClass('menu__side-active');
-    $('.menu__side').removeClass('menu__side-active');
-    $('.menu').removeClass('menu__active');
-  }
-
-  /*ВЫПЕЧКА*/
-  /*маджио(maj)*/
-  $(document).on('mouseover', '.maj', function(e){
-    e.preventDefault;
-    $('.maj-t').addClass('menu__side-active');
-    $('.menu__side').addClass('menu__side-active');
-    $('.menu').addClass('menu__active');
-  }
-
-  $(document).on('mouseout', '.maj', function(e){
-    e.preventDefault;
-    $('.maj-t').removeClass('menu__side-active');
-    $('.menu__side').removeClass('menu__side-active');
-    $('.menu').removeClass('menu__active');
-  }
-
-  /*круассан с шк и фнд(krsh)*/
-  $(document).on('mouseover', '.krsh', function(e){
-    e.preventDefault;
-    $('.krsh-t').addClass('menu__side-active');
-    $('.menu__side').addClass('menu__side-active');
-    $('.menu').addClass('menu__active');
-  }
-
-  $(document).on('mouseout', '.krsh', function(e){
-    e.preventDefault;
-    $('.krsh-t').removeClass('menu__side-active');
-    $('.menu__side').removeClass('menu__side-active');
-    $('.menu').removeClass('menu__active');
-  }
-
-  /*ПИРОГИ*/
-  /*лималина(lmln)*/
-  $(document).on('mouseover', '.lmln', function(e){
-    e.preventDefault;
-    $('.lmln-t').addClass('menu__side-active');
-    $('.menu__side').addClass('menu__side-active');
-    $('.menu').addClass('menu__active');
-  }
-
-  $(document).on('mouseout', '.lmln', function(e){
-    e.preventDefault;
-    $('.lmln-t').removeClass('menu__side-active');
-    $('.menu__side').removeClass('menu__side-active');
-    $('.menu').removeClass('menu__active');
-  }
-  /*шоколадный(shok)*/
-
-
-  $(document).on('mouseover', '.shok', function(e){
-    e.preventDefault;
-    $('.shok-t').addClass('menu__side-active');
-    $('.menu__side').addClass('menu__side-active');
-    $('.menu').addClass('menu__active');
-  }
-
-  $(document).on('mouseout', '.shok', function(e){
-    e.preventDefault;
-    $('.shok-t').removeClass('menu__side-active');
     $('.menu__side').removeClass('menu__side-active');
     $('.menu').removeClass('menu__active');
   }
