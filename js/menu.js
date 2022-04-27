0e93554517ceb8a4d2fd8fe036df76f477820fa2
majorTtom <69448481+majorTtom@users.noreply.github.com>
2022-04-27 03:08:38 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -99,6 +99,132 @@ $(document).ready(function(){
     $('.menu').removeClass('menu__active');
   })
 
+  /*ТОРТЫ*/
+  /*меренговый рулет(mr)*/
+  $(document).on('mouseenter', '.mr', function(e){
+    e.preventDefault;
+    $('.mr-t').addClass('menu__side-active');
+    $('.menu__side').addClass('menu__side-active');
+    $('.menu').addClass('menu__active');
+  })
+
+  $(document).on('mouseleave', '.mr', function(e){
+    e.preventDefault;
+    $('.mr-t').removeClass('menu__side-active');
+    $('.menu__side').removeClass('menu__side-active');
+    $('.menu').removeClass('menu__active');
+  })
+
+  /*наролеон(nap)*/
+  $(document).on('mouseenter', '.nap', function(e){
+    e.preventDefault;
+    $('.nap-t').addClass('menu__side-active');
+    $('.menu__side').addClass('menu__side-active');
+    $('.menu').addClass('menu__active');
+  })
+
+  $(document).on('mouseleave', '.nap', function(e){
+    e.preventDefault;
+    $('.nap-t').removeClass('menu__side-active');
+    $('.menu__side').removeClass('menu__side-active');
+    $('.menu').removeClass('menu__active');
+  })
+
+  /*ГРАНОЛА*/
+  /*gr1*/
+  $(document).on('mouseenter', '.gr1', function(e){
+    e.preventDefault;
+    $('.gr1-t').addClass('menu__side-active');
+    $('.menu__side').addClass('menu__side-active');
+    $('.menu').addClass('menu__active');
+  })
+
+  $(document).on('mouseleave', '.gr1', function(e){
+    e.preventDefault;
+    $('.gr1-t').removeClass('menu__side-active');
+    $('.menu__side').removeClass('menu__side-active');
+    $('.menu').removeClass('menu__active');
+  })
+
+  /*gr6*/
+  $(document).on('mouseenter', '.gr6', function(e){
+    e.preventDefault;
+    $('.gr6-t').addClass('menu__side-active');
+    $('.menu__side').addClass('menu__side-active');
+    $('.menu').addClass('menu__active');
+  })
+
+  $(document).on('mouseleave', '.gr6', function(e){
+    e.preventDefault;
+    $('.gr6-t').removeClass('menu__side-active');
+    $('.menu__side').removeClass('menu__side-active');
+    $('.menu').removeClass('menu__active');
+  })
+
+  /*ВЫПЕЧКА*/
+  /*маджио(maj)*/
+  $(document).on('mouseenter', '.maj', function(e){
+    e.preventDefault;
+    $('.maj-t').addClass('menu__side-active');
+    $('.menu__side').addClass('menu__side-active');
+    $('.menu').addClass('menu__active');
+  })
+
+  $(document).on('mouseleave', '.maj', function(e){
+    e.preventDefault;
+    $('.maj-t').removeClass('menu__side-active');
+    $('.menu__side').removeClass('menu__side-active');
+    $('.menu').removeClass('menu__active');
+  })
+
+  /*круассан с шк и фнд(krsh)*/
+  $(document).on('mouseenter', '.krsh', function(e){
+    e.preventDefault;
+    $('.krsh-t').addClass('menu__side-active');
+    $('.menu__side').addClass('menu__side-active');
+    $('.menu').addClass('menu__active');
+  })
+
+  $(document).on('mouseleave', '.krsh', function(e){
+    e.preventDefault;
+    $('.krsh-t').removeClass('menu__side-active');
+    $('.menu__side').removeClass('menu__side-active');
+    $('.menu').removeClass('menu__active');
+  })
+
+  /*ПИРОГИ*/
+  /*лималина(lmln)*/
+  $(document).on('mouseenter', '.lmln', function(e){
+    e.preventDefault;
+    $('.lmln-t').addClass('menu__side-active');
+    $('.menu__side').addClass('menu__side-active');
+    $('.menu').addClass('menu__active');
+  })
+
+  $(document).on('mouseleave', '.lmln', function(e){
+    e.preventDefault;
+    $('.lmln-t').removeClass('menu__side-active');
+    $('.menu__side').removeClass('menu__side-active');
+    $('.menu').removeClass('menu__active');
+  })
+  /*шоколадный(shok)*/
+
+
+  $(document).on('mouseenter', '.shok', function(e){
+    e.preventDefault;
+    $('.shok-t').addClass('menu__side-active');
+    $('.menu__side').addClass('menu__side-active');
+    $('.menu').addClass('menu__active');
+  })
+
+  $(document).on('mouseleave', '.shok', function(e){
+    e.preventDefault;
+    $('.shok-t').removeClass('menu__side-active');
+    $('.menu__side').removeClass('menu__side-active');
+    $('.menu').removeClass('menu__active');
+  })
+
+
 })
 
 
